c9171a5fe9fc1135570afdeef9305c9de7110b81
Nick Silkey <nick.silkey@rackspace.com>
2014-11-03 11:52:27 -0600

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'designate-postfix'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures designate-postfix'
+long_description 'Installs/Configures designate-postfix'
+version          '0.1.0'
+
